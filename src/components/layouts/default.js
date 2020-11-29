be4c524559afae1c06eeca68be89fcc01786e757
Brady Kurtz <bradykurtz@churchofjesuschrist.org>
2020-11-29 11:29:57 -0700

    Upgrading starter to typescript

--- src/components/layouts/default.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const DefaultLayout = ({ children }) => (
-    <div className="default-container">
-        { children }
-    </div>
-);
-
-export default DefaultLayout;
